5b31f8ed2dbef939f2b1579fb62dc8811a866cd7
Priyanshu3369 <priyanshuchourasiya32198@gmail.com>
2025-05-21 22:03:31 +0530

    done

--- app.py
+++ app.py
@@ -652,5 +652,6 @@ def view_diary_entry(date):
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
